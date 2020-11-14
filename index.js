c99909204f2783303a110c5b0e7d0d53b720a626
Alysson Eduardo <alysson6801@hotmail.com>
2020-11-14 16:27:19 -0300

    (14/11) Inicio do Projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+//importando modulo
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'))
+
+//Configurando EJS no Express
+//Passando para o express que o motor usado - view engine - é o ejs
+app.set('view engine', 'ejs');
+
+//rota de teste
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.get('/perguntar', (req, res) => res.render('perguntar'))
+
+//Rodando Servidor
+app.listen(8080, ()=> console.log("App rodando!!!"))
